6fbb33d059d347171eeecb25dc64afc10edfd865
Alex Mickelson <alexmickelson96@gmail.com>
2023-01-26 01:05:46 +0000

    can view assignment status

--- api/src/features/assignment_router.py
+++ api/src/features/assignment_router.py
@@ -1,3 +1,4 @@
+from typing import Dict
 from fastapi import APIRouter
 from pydantic import BaseModel
 from src.services import braille_service
@@ -6,6 +7,8 @@ router = APIRouter(prefix="/assignments")
 
 
 assignments = [{"id": 0, "name": "Assignment 1", "text": "translate this"}]
+
+grades: Dict[int, None | float] = {0: None}
 
 
 @router.get("/details/{assignment_id}")
@@ -19,10 +22,13 @@ class AssignmentSubmission(BaseModel):
 
 @router.post("/submit/{assignment_id}")
 async def submit_assignment(assignment_id: int, body: AssignmentSubmission):
+    global grades
     translated_brail_submission = braille_service.braille_to_text(body.brail)
     if translated_brail_submission == assignments[assignment_id]["text"]:
+        grades[assignment_id] = 100.0
         return {"correctly_translated": True}
     else:
+        grades[assignment_id] = 0.0
         return {
             "correctly_translated": False,
             "actual_translation": translated_brail_submission,
@@ -32,3 +38,15 @@ async def submit_assignment(assignment_id: int, body: AssignmentSubmission):
 @router.get("/all")
 async def get_all_assignments():
     return assignments
+
+
+@router.get("/grade/{assignment_id}")
+async def get_assignment_grade():
+    global grades
+    return {"grade": grades[0]}
+
+
+@router.delete("/grades/all")
+async def delete_all_assignments():
+    global grades
+    grades = {0: None}
--- api/src/tests/test_assignments/test_assignments.py
+++ api/src/tests/test_assignments/test_assignments.py
@@ -13,15 +13,20 @@ def test_can_get_first_assignment():
 
 
 def test_can_translate_submitted_assignment():
-    assignment_text = "translate this"
-    assignment_submisison = {"brail": "⠞⠗⠁⠝⠎⠇⠁⠞⠑⠀⠞⠓⠊⠎"}
-    url = "/api/assignments/submit/0"
     client = TestClient(app)
-    response = client.post(url, json=assignment_submisison)
-    assert response.is_success
+    response = correctly_submit_assignment(client)
 
     expected = {"correctly_translated": True}
     assert response.json() == expected
+
+
+def correctly_submit_assignment(client: TestClient):
+    assignment_text = "translate this"
+    assignment_submisison = {"brail": "⠞⠗⠁⠝⠎⠇⠁⠞⠑⠀⠞⠓⠊⠎"}
+    url = "/api/assignments/submit/0"
+    response = client.post(url, json=assignment_submisison)
+    assert response.is_success
+    return response
 
 
 def test_incorrect_brail_is_marked_incorrect():
@@ -38,9 +43,32 @@ def test_incorrect_brail_is_marked_incorrect():
 def test_can_get_assignment_list():
     url = "/api/assignments/all"
     client = TestClient(app)
-    
+
     response = client.get(url)
     assert response.is_success
 
     expected = [{"id": 0, "name": "Assignment 1", "text": "translate this"}]
+    assert response.json() == expected
+
+
+def test_can_view_assignment_grade():
+    client = TestClient(app)
+    client.delete('/api/assignments/grades/all')
+    url = "/api/assignments/grade/0"
+    response = client.get(url)
+    assert response.is_success
+
+    expected = {"grade": None}
+    assert response.json() == expected
+
+
+def test_submitted_assignments_have_grades():
+    client = TestClient(app)
+    correctly_submit_assignment(client)
+
+    url = "/api/assignments/grade/0"
+    response = client.get(url)
+    assert response.is_success
+
+    expected = {"grade": 100.0}
     assert response.json() == expected
